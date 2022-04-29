ce9414b3ddd1bb5ae9ecaf317e061a206855a20b
miichaels <michael_araujo2012@hotmail.com>
2022-04-29 15:16:15 -0300

    Atividade CRUD back-end

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/PostagemModel.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/PostagemModel.java
@@ -1,10 +1,14 @@
 package com.generation.blogpessoal.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -19,42 +23,66 @@ public class PostagemModel {
 	
 	// Equivalente ao auto_increment no mysql
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	public long id;
+	public Long id;
 	
 	// Define que o campo é obrigatorio
 	@NotNull
 	@Size(min = 5, max = 100, message = "O campo deve ter no minimo 5 caracteres e no maximo 100 caracteres")
 	public String titulo;
 	
+
+
 	// Define que o campo é obrigatorio
 	@NotNull
 	public String texto;
 	
 	
-	public long getId() {
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+
+
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+
+	public void setId(Long id) {
 		this.id = id;
 	}
+
 
 	public String getTitulo() {
 		return titulo;
 	}
 
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 
 	public String getTexto() {
 		return texto;
 	}
 
+
 	public void setTexto(String texto) {
 		this.texto = texto;
-		
 	}
+
+
+	public Date getData() {
+		return data;
+	}
+
+
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
+	
+
 	
 	
 }
--- blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.generation.blogpessoal.model.PostagemModel;
 
+// É necessaria por boas praticas/validacao do robo
 @Repository
 public interface PostagemRepository extends JpaRepository<PostagemModel, Long>{
 	public List<PostagemModel> findAllByTituloContainingIgnoreCase (String titulo); // Buscar todos pelo titulo da entidade
