2eaaaccba15aa0ab28ab4c3149eb1ee1e34efe02
miichaels <michael_araujo2012@hotmail.com>
2022-05-02 17:08:30 -0300

    Atividade CRUD BACK-END

--- blogpessoal/src/main/java/com/generation/blogpessoal/model/PostagemModel.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/model/PostagemModel.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity //Faz o objeto virar uma table no BD
 
@@ -30,6 +33,18 @@ public class PostagemModel {
 	@Size(min = 5, max = 100, message = "O campo deve ter no minimo 5 caracteres e no maximo 100 caracteres")
 	public String titulo;
 	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+	public Tema getTema() {
+		return tema;
+	}
+
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 
 	// Define que o campo é obrigatorio
